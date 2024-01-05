d2a1974f7551b59de069cc858b7902634e5567fb
Lusca-rcg <lusca.rcg@gmail.com>
2024-01-05 17:16:18 -0300

    feat: created post

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
 const express = require ('express');
 const bodyParser = require ('body-parser');
+const uuid= require('uuid');
 const app = express();
+const task = []
+const port = 3000
 
 app.use(bodyParser.json());
 
-app.listen(3000,() => {
+app.listen(port,() => {
     console.log('Server is running... http://localhost:3000')
 })
 
 app.get("/", (req,res) => {
-    res.json({ message: "Hello, World!"})
+    res.json(task)
+})
+
+app.post('/',(req,res) => {
+
+    const {id, nome, descricao, status} = req.body
+    const newId = uuid.v4()
+    task.id = newId
+   
+    task.push({
+       
+        nome: nome,
+        decricao: descricao,
+        status: status,
+         id: newId
+        })
+    res.json({ task })
+    
+        
 })
