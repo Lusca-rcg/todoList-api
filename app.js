dbc029f2204f064d68053a76a32661284aed9f84
Lusca-rcg <lusca.rcg@gmail.com>
2024-01-05 15:34:20 -0300

    feat: created get test

--- app.js
+++ app.js
@@ -7,3 +7,7 @@ app.use(bodyParser.json());
 app.listen(3000,() => {
     console.log('Server is running... http://localhost:3000')
 })
+
+app.get("/", (req,res) => {
+    res.json({ message: "Hello, World!"})
+})
