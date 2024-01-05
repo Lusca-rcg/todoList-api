40ede1cffa4bbd28262d0b0703f355bbfc8f9073
Lusca-rcg <lusca.rcg@gmail.com>
2024-01-05 19:48:28 -0300

    feat: created put

--- app.js
+++ app.js
@@ -1,47 +1,70 @@
-const express = require ('express');
-const bodyParser = require ('body-parser');
-const uuid= require('uuid');
+const express = require('express');
+const bodyParser = require('body-parser');
+const uuid = require('uuid');
 const app = express();
 const tasks = []
 const port = 3000
 
 app.use(bodyParser.json());
 
-app.listen(port,() => {
+app.listen(port, () => {
     console.log('Server is running... http://localhost:3000')
 })
 
-app.get("/", (req,res) => {
+app.get("/", (req, res) => {
     res.json(tasks)
 })
 
-app.post('/',(req,res) => {
+app.post('/', (req, res) => {
 
-    const {id, nome, descricao, status} = req.body
+    const { id, nome, descricao, status } = req.body
     const newId = uuid.v4()
     tasks.id = newId
-   
+
     tasks.push({
-       
+
         nome: nome,
         decricao: descricao,
         status: status,
         id: newId
-        })
+    })
     res.json({ tasks })
-    
-        
+
+
 })
 
-app.delete('/:id',(req,res) =>{
+app.put('/:id', (req, res) => {
+    const taskId = req.params.id
+    const {nome, descricao, status} = req.body
+
+    const taskIndex = tasks.findIndex((task) => task.id === taskId)       
+
+    if (taskIndex !== -1) {
+        
+        tasks[taskIndex] = {
+            //...tasks[taskIndex],
+            nome: nome,
+            descricao: descricao,
+            status: status
+        }
+        
+        res.json({ message: "Tarefa Atualizada.", task: tasks[taskIndex] })
+    }
+    else {
+        res.status(404).json({ message: "Tarefa nao encontrada" })
+    }
+})
+
+app.delete('/:id', (req, res) => {
     const taskId = req.params.id
     const taskIndex = tasks.findIndex((task) => task.id === taskId)
-    
-    if (taskIndex.splice !== -1){
+
+    if (taskIndex.splice !== -1) {
         tasks.splice(taskIndex, 1)
-        res.json({message: "Tarefa removida."})
-    } else{
-        res.status(404).json({message: "Tarefa nao encontrada"})
+        res.json({ message: "Tarefa removida." })
+    }
+    else {
+        res.status(404).json({ message: "Tarefa nao encontrada" })
     }
 
 })
